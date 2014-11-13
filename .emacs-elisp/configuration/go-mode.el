be8b1c639a06f0ce04280a372f566a4601d3b348
GallonChoi <404842441@qq.com>
2014-11-13 13:25:25 +0800

    Bug fixes: go-mode with autopair-mode

--- .emacs-elisp/configuration/go-mode.el
+++ .emacs-elisp/configuration/go-mode.el
@@ -1,7 +1,7 @@
 ;; ----------------- go-mode ----------------------
 ;; Tutorial: http://yousefourabi.com/blog/2014/05/emacs-for-go/
 
-(add-to-list 'load-path "~/.emacs-elisp/go-mode.el")
+(add-to-list 'load-path "~/.emacs-elisp")
 (require 'go-mode)
 
 ;; Keybindings
@@ -11,16 +11,18 @@
 ;; "C-c C-f": Format the buffer without saving
 ;; "C-c C-k": Show the go documentation for a given package
 ;; ------------------------------------------------
+
 (add-hook 'go-mode-hook '(lambda ()
-                           (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)))
-(add-hook 'go-mode-hook '(lambda ()
-                           (local-set-key (kbd "C-c C-g" 'go-goto-imports))))
-(add-hook 'go-mode-hook '(lambda ()
-                           (local-set-key (kbd "C-c C-f" 'gofmt))))
-(add-hook 'before-save-hook 'gofmt-before-save)
-(add-hook 'go-mode-hook '(lambda ()
+                           (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
+                           (local-set-key (kbd "C-c C-g" 'go-goto-imports))
+                           (local-set-key (kbd "C-c C-f" 'gofmt))
                            (local-set-key (kbd "C-c C-k") 'godoc)))
 
+(add-hook 'before-save-hook 'gofmt-before-save)
 ;; go-flymake
 (add-to-list 'load-path "~/.emacs-elisp/goflymake")
 (require 'go-flymake)
+;; FIXME: go-mode with autopair
+(add-hook 'go-mode-hook '(lambda ()
+                           (local-set-key (kbd "C-c C-e") 'flymake-display-err-menu-for-current-line)
+                           (cc-common-mode-auto-pair)))

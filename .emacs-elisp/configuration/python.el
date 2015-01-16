333c5ee330b88d85d113c7612b88bf62c7b48df7
GallonChoi <404842441@qq.com>
2015-01-16 23:32:39 +0800

    set keybinding to py-pep8-run

--- .emacs-elisp/configuration/python.el
+++ .emacs-elisp/configuration/python.el
@@ -8,7 +8,7 @@
 (autoload 'jedi:setup "jedi" nil t)
 (add-hook 'python-mode-hook 'jedi:setup)
 (define-key python-mode-map (kbd "C-3") 'comment-or-uncomment-region)
-
+(define-key python-mode-map (kbd "C-c C-a") 'py-pep8-run)
 
 ;;; (set-face-foreground 'py-variable-name-face "#74E32D")
 ;; (set-face-foreground 'py-variable-name-face "#FF7F50")

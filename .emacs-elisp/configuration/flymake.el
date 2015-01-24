67cc7f94bc139deb467e1fb56e69ca189be58f4e
GallonChoi <404842441@qq.com>
2015-01-24 14:40:13 +0800

    martparens-mode

--- .emacs-elisp/configuration/flymake.el
+++ .emacs-elisp/configuration/flymake.el
@@ -1,7 +1,7 @@
 ;; Show error in mini-buffer
 (defun my-flymake-show-help ()
   (when (get-char-property (point) 'flymake-overlay)
-   (let ((help (get-char-property (point) 'help-echo)))
-    (if help (message "%s" help)))))
+    (let ((help (get-char-property (point) 'help-echo)))
+      (if help (message "%s" help)))))
 
 (add-hook 'post-command-hook 'my-flymake-show-help)

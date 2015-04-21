0d56c6c7a3de728fe911c495658daa38cab75618
GallonChoi <404842441@qq.com>
2015-04-21 11:56:09 +0000

    add js2-mode module

--- .emacs-elisp/configuration/js2-mode.el
+++ .emacs-elisp/configuration/js2-mode.el
@@ -1,6 +1,7 @@
 ;;-------------------- js2-mode -------------------
+(add-to-list 'load-path "~/.emacs-elisp/js2-mode/")
 (require 'js2-mode)
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode)) 
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
 ;;------------------- jshint ----------------------
 (require 'flymake-jshint)

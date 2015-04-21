abd4769312be103a608d570c59b6ca5a519b8b23
GallonChoi <404842441@qq.com>
2015-04-21 12:30:05 +0000

    add yafolding module

--- .emacs-elisp/configuration/yafolding.el
+++ .emacs-elisp/configuration/yafolding.el
@@ -1,11 +1,5 @@
 ;;---------------yafolding-----------------
+(add-to-list 'load-path "~/.emacs-elisp/yafolding.el")
 (require 'yafolding)
-(define-key global-map (kbd "C-;") 'yafolding)
-;;(define-key global-map (kbd "C-c C-f") 'yafolding-toggle-all)
-(define-key global-map (kbd "C-c C-f") 'yafolding-toggle-all-by-current-level)
-;;(add-hook 'indent-buffer-before-hook
-;;    (lambda ()
-;;      (yafolding-temp-toggle nil)))
-;;(add-hook 'indent-buffer-after-hook
-;;    (lambda ()
-;;      (yafolding-temp-toggle t)))
+(define-key global-map (kbd "C-'") 'yafolding-toggle-element)
+(define-key global-map (kbd "C-c C-f") 'yafolding-toggle-all)

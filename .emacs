0d721337fb7f7200e0b1405bcd45fbeb86a51abf
GallonChoi <404842441@qq.com>
2015-04-21 23:24:27 +0000

    restruct files init

--- .emacs
+++ .emacs
@@ -18,108 +18,80 @@
 ;;Personal message
 (setq user-full-name "Gallon Choi")
 (setq user-mail-address "gallonq@gmail.com")
+(setq default-direct "~/Workspace/")
 
+;; load-path
 (add-to-list 'load-path "~/.emacs-elisp/common/dash.el/")
+(add-to-list 'load-path "~/.emacs-elisp/common/helm")
+(add-to-list 'load-path "~/.emacs-elisp/common/undo-tree")
+(add-to-list 'load-path "~/.emacs-elisp/common/hungry-delete")
+;; before conf
 (require 'dash)
-(add-to-list 'load-path (expand-file-name "~/.emacs-elisp"))
-(setq my-auto-lisp (list "~/.emacs-elisp/configuration"))
-(require 'load-files)
 
-;;-------------------- Smart Compile --------------------
-;;    Load the smart-compile+ .                         .
-;;-------------------------------------------------------
-(add-to-list 'load-path "~/.emacs-elisp/lang/c/smart-compile-plus/")
-(require 'smart-compile+)
-(global-set-key [(f9)] 'smart-compile)
+;; configuration
+(load "~/.emacs-elisp/configuration/AC-yas.el")
+(load "~/.emacs-elisp/configuration/auto-header.el")
+(load "~/.emacs-elisp/configuration/auto-pair.el")
+(load "~/.emacs-elisp/configuration/cc-mode.el")
+(load "~/.emacs-elisp/configuration/decode.el")
+(load "~/.emacs-elisp/configuration/flymake.el")
+(load "~/.emacs-elisp/configuration/general.el")
+(load "~/.emacs-elisp/configuration/js2-mode.el")
+(load "~/.emacs-elisp/configuration/org.el")
+(load "~/.emacs-elisp/configuration/python.el")
+(load "~/.emacs-elisp/configuration/scala.el")
+(load "~/.emacs-elisp/configuration/web-mode.el")
+(load "~/.emacs-elisp/configuration/yafolding.el")
+(load "~/.emacs-elisp/configuration/theme.el")
 
-;;-------------------- gdb设置 --------------------
-;;     多窗口gdb
-;; ------------------------------------------------
-(setq gdb-many-windows t)
-(setq gdb-use-separate-io-buffer t)
+;; key bingdings
+;;===================================================
+(global-set-key (kbd "M-x") 'helm-M-x)  ;; helm
+(global-set-key (kbd "C-=") 'eshell)
+(global-set-key [f6] 'speedbar)
+;; (setq-default indent-tabs-mode nil) ;;Disable TAB triggering indent
+;;Set mark
+(global-unset-key (kbd "C-SPC"))
+(global-set-key (kbd "M-SPC") 'set-mark-command)
+;;Eval-region
+(define-key emacs-lisp-mode-map (kbd "C-x C-r") 'eval-region)
+(define-key lisp-interaction-mode-map (kbd "C-x C-r") 'eval-region)
+;;Code check(flymake)
+(global-set-key (kbd "C-c u") 'flymake-mode)
 
-;;-------------------- clang-completion-mode --------------------
-(add-to-list 'load-path "~/.emacs-elisp/lang/c/")
-(load-library "clang-completion-mode")
+(global-set-key [mouse-4] 'down-slightly)
+(global-set-key [mouse-5] 'up-slightly)
+(global-set-key (kbd "<C-mouse-4>") 'text-scale-increase)
+(global-set-key (kbd "<C-mouse-5>") 'text-scale-decrease)
 
-;;-------------------- 重定义注释方法 --------------------
-;; 没有任中任何区域时，注释、反注释作用于当前行，否则作用于
-;; 选中区域
-;;-----------------------------------------------------
-(defadvice comment-or-uncomment-region (before slickcomment activate compile)
-  "When called interactively with no active region, toggle comment on current line instead."
-  (interactive
-   (if mark-active (list (region-beginning) (region-end))
-     (list (line-beginning-position)
-           (line-beginning-position 2)))))
+(global-set-key [f11] 'toggle-fullscreen)
+
+;;gdb
+(global-set-key [(f8)] 'gud-step)
+(global-set-key [(f7)] 'gud-next)
+(global-set-key [(f4)] 'gud-until);;Recover kmacro-end-or-call-macro
+(global-set-key [(f12)] 'gud-remove)
+(global-set-key (kbd "C-c f") 'gud-finish)
+(global-set-key (kbd "C-c s")'gud-start)
+(global-set-key (kbd "C-c g")'gud-go)
+(global-set-key (kbd "C-c t")'gud-cont)
+(global-set-key (kbd "C-c w")'gud-watch)
+
+;; indent
+(global-set-key (kbd "C-c C-i") 'indent-region-or-buffer)
+(global-set-key (kbd "C-j")'newline-and-indent)
+
+;; expand region
+(global-set-key (kbd "C-c RET") 'er/expand-region)
+
+;; comment or uncomment
 (global-set-key (kbd "C-.") 'comment-or-uncomment-region)
 
-;;-------------------- 重定义删除行 --------------------
-;;     T_T目前不知道作用
-;;     When called interactively with no active
-;;     region, copy a single line instead.When
-;;     called interactively with no active region,
-;;     kill a single line instead.”
-;;----------------------------------------------------
-(defadvice kill-ring-save (before slickcopy activate compile)
-  (interactive
-   (if mark-active (list (region-beginning) (region-end))
-     (list (line-beginning-position)
-           (line-beginning-position 2)))))
-(defadvice kill-region (before slickcut activate compile)
-  (interactive
-   (if mark-active (list (region-beginning) (region-end))
-     (list (line-beginning-position)
-           (line-beginning-position 2)))))
-
-
-;;-------------------- html-helper-mode --------------------
-;;    Set up the html-helper-mode.                          .
-;;-----------------------------------------------------------
-(setq html-helper-build-new-buffer nil)
-(setq html-helper-address-string "<a href= \"http://gallon.cc\">G_allon<\\a>")
-
-(global-font-lock-mode 1)
-;;; Also highlight parens
-(setq show-paren-delay 0
-      show-paren-style 'parenthesis)
-(show-paren-mode 1)
-;;; This is the binary name of my scheme implementation
-(setq scheme-program-name "mit-scheme")
-
+;; hungry delete
 (global-set-key (kbd "C-M-y") 'query-replace-regexp)
 (global-set-key (kbd "M-.") 'delete-horizontal-space)
-;; Hungry-delete
-(add-to-list 'load-path "~/.emacs-elisp/common/hungry-delete")
-(require 'hungry-delete)
-(global-hungry-delete-mode)
 (global-set-key (kbd "C-c C-d") 'hungry-delete-forward)
 (global-set-key (kbd "C-c DEL") 'hungry-delete-backward)
 
-;; ====================
-;; insert date and time
-
-(defvar current-date-time-format "%a %b %d %H:%M:%S %Z %Y"
-  "Format of date to insert with `insert-current-date-time' func
-See help of `format-time-string' for possible replacements")
-
-(defvar current-time-format "%a %H:%M:%S"
-  "Format of date to insert with `insert-current-time' func.
-Note the weekly scope of the command's precision.")
-
-(defun insert-current-date-time ()
-  "insert the current date and time into current buffer.
-Uses `current-date-time-format' for the formatting the date/time."
-       (interactive)
-;       (insert (let () (comment-start)))
-       (insert (format-time-string current-date-time-format (current-time)))
-       )
-
-(defun insert-current-time ()
-  "insert the current time (1-week scope) into the current buffer."
-       (interactive)
-       (insert (format-time-string current-time-format (current-time)))
-       )
-
-(global-set-key "\C-c\C-t" 'insert-current-date-time)
-;; (global-set-key "\C-c\C-t" 'insert-current-time)
+;; insert date time
+(global-set-key (kbd "C-c C-t") 'insert-current-date-time)
--- .emacs-elisp/configuration/general.el
+++ .emacs-elisp/configuration/general.el
@@ -1,109 +1,76 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;   Filename:    general.el
-;;   Version:     1.0
-;;
-;;   Description: General Configuration for Emacs
-;;
-;;   Created at:  Mon Jun 23 21:53:01 2014
-;;
-;;   Gallon Choi is a student majoring in Software Engineering,
-;;   from the School of Computer,
-;;   Guangdong University of Technology, GZ 510006, P. R. China
-;;
-;;   Status:      Experimental, do not distribute.
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(setq default-direct "~/Workspace/")
-;;Line number
-(global-linum-mode t)
-;;speedbar
-(global-set-key [f6] 'speedbar)
-;;Disable TAB triggering indent
-(setq-default indent-tabs-mode nil)
-;;eshell
-(global-set-key [(control =)] 'eshell)
+;;Set title
+(setq frame-title-format "%f@emacs")
 
+;;
+(global-linum-mode t)
+
+;;
 (fset 'yes-or-no-p 'y-or-n-p)
 
-;; Smex
-;; (autoload 'smex "smex"
-;; "Smex is a M-x enhancement for Emacs, it provides a convenient interface to
-;; your recently and most frequently used commands.")
-;; (global-set-key (kbd "M-x") 'smex)
-;; Helm (Helm is better than smex)
-(add-to-list 'load-path "~/.emacs-elisp/common/helm")
+;; Helm
 (require 'helm-config)
-(global-set-key (kbd "M-x") 'helm-M-x)
-(helm-mode 1)
+(helm-mode t)
+(helm-autoresize-mode t)
 
-;;ido-mode cooporate with smex
-;; (ido-mode t)
 ;;Set Chinese punctuation
 (setq sentence-end "\\([。！？]\\|……\\|[.?!][]\"')}]*\\($\\|[ \t]\\)\\)[ \t\n]*")
 (setq sentence-end-double-space nil)
 
 ;;Disable scroll-bar
 (customize-set-variable 'scroll-bar-mode nil)
+
 ;;Disable making temporary files
 (setq-default make-backup-file nil)
+
 ;;Disable startup-message
 (setq inhibit-startup-message t)
+
 ;;Bracket pair
 (show-paren-mode t)
+
 ;;Tool and menu bar
 (tool-bar-mode 0)
 (menu-bar-mode 0)
-;;Highlight
-(global-font-lock-mode t)
-;;Highlight the chosen charactor
-(transient-mark-mode t)
-;;Default Text Mode
-(setq default-major-mode 'text-mode)
-;;Support copy and paste from outside
-(setq x-select-enable-clipboard t)
-;;C-k delete the whole line
-(setq-default kill-whole-line t)
-;;kill ring max: 200
-(setq kill-ring-max 200)
-;;Tab width
-(setq default-tab-width 4)
+
 ;;Disable scroll bar
 (set-scroll-bar-mode nil)
+
+;;Highlight
+(global-font-lock-mode t)
+
+;;Highlight the chosen charactor
+(transient-mark-mode t)
+
+;;Default Mode
+(setq default-major-mode 'text-mode)
+
+;;Support copy and paste from outside
+(setq x-select-enable-clipboard t)
+
+;;C-k delete the whole line
+(setq-default kill-whole-line t)
+
+;;kill ring max: 200
+(setq kill-ring-max 200)
+
+;;Tab width
+(setq default-tab-width 4)
+
 ;;Prevent jumping while scrolling
 (setq redisplay-dont-pause t
       scroll-step 1
       scroll-margin 5
       scroll-conservatively 10000
       scroll-preserve-screen-position 1)
+
 ;;Show image
-(auto-image-file-mode)
-;;Set title
-(setq frame-title-format "%f@emacs")
-;;Set mark
-(global-unset-key (kbd "C-SPC"))
-(global-set-key (kbd "M-SPC") 'set-mark-command)
-;;Eval-region
-(define-key emacs-lisp-mode-map (kbd "C-x C-r") 'eval-region)
-(define-key lisp-interaction-mode-map (kbd "C-x C-r") 'eval-region)
-;;Code check
-(global-set-key (kbd "C-c u") 'flymake-mode)
-;;Common display setting
-;;=============================================================
-;;time
-(setq display-time-24hr-format t)
-(setq display-time-format "%02H:%02M:%02S %Y-%02m-%02d %3a")
-(setq display-time-interval 1);;时间更新频率
-(display-time-mode 1)
-;;column number
-(column-number-mode t)
-(size-indication-mode t)
-;;calendar color
-(setq calendar-load-hook
-      '(lambda ()
-         (set-face-foreground 'diary-face "skyblue")
-         (set-face-background 'holiday-face "slate blue")
-         (set-face-foreground 'holiday-face "white")))
-;;=============================================================
+(auto-image-file-mode t)
+
+(global-font-lock-mode 1)
+;;; Also highlight parens
+(setq show-paren-delay 0
+      show-paren-style 'parenthesis)
+(show-paren-mode 1)
 
 ;;Cursor
 ;;==============================================================
@@ -112,8 +79,6 @@
 ;;Scroll line : 3
 (defun up-slightly () (interactive) (scroll-up 3))
 (defun down-slightly () (interactive) (scroll-down 3))
-(global-set-key [mouse-4] 'down-slightly)
-(global-set-key [mouse-5] 'up-slightly)
 ;;==============================================================
 
 ;;Find the next nearest charactor and jump to it
@@ -144,7 +109,7 @@ occurence of CHAR."
 
 ;;Full screen
 ;;It seems not work well with Gnome or gtk?
-;;============================================================================
+;;==========================================================================
 (defun toggle-fullscreen (&optional f)
   (interactive)
   (let ((current-value (frame-parameter nil 'fullscreen)))
@@ -153,35 +118,13 @@ occurence of CHAR."
                              (if (boundp 'old-fullscreen) old-fullscreen nil)
                            (progn (setq old-fullscreen current-value)
                                   'fullboth)))))
-(global-set-key [f11] 'toggle-fullscreen)
 (add-hook 'emacs-startup-hook 'toggle-fullscreen)
 ;;============================================================================
 
 ;; Undo-Tree
-;; Trigger-key: C-x u
-(add-to-list 'load-path "~/.emacs-elisp/common/undo-tree")
 (require 'undo-tree)
 (global-undo-tree-mode t)
 
-;;==========================================================
-;; ==================== 滚动缩放 ====================
-;; http://zhuoqiang.me/torture-emacs.html
-;; For Linux
-(global-set-key (kbd "<C-mouse-4>") 'text-scale-increase)
-(global-set-key (kbd "<C-mouse-5>") 'text-scale-decrease)
-
-;;gdb按键设置
-;;==========================================================
-(global-set-key [(f8)] 'gud-step)
-(global-set-key [(f7)] 'gud-next)
-(global-set-key [(f4)] 'gud-until);;Recover kmacro-end-or-call-macro
-(global-set-key [(f12)] 'gud-remove)
-(global-set-key "\C-cf" 'gud-finish)
-(global-set-key "\C-cs" 'gud-start)
-(global-set-key "\C-cg" 'gud-go)
-(global-set-key "\C-ct" 'gud-cont)
-(global-set-key "\C-cw" 'gud-watch)
-;;==========================================================
 ;; Indent region or buffer
 (defun indent-buffer ()
   "Indent the currently visited buffer."
@@ -201,10 +144,9 @@ occurence of CHAR."
         (message "Indented buffer.")))
     (whitespace-cleanup)
     (untabify (point-min) (point-max))))
-(global-set-key (kbd "C-c C-i") 'indent-region-or-buffer)
 
-(global-set-key "\C-j" 'newline-and-indent)
 
+;; package
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
@@ -216,7 +158,42 @@ occurence of CHAR."
 
 ;; expand-region
 (require 'expand-region)
-(global-set-key (kbd "C-c RET") 'er/expand-region)
 
 ;; restclient
 (require 'restclient)
+
+;;
+(defadvice comment-or-uncomment-region (before slickcomment activate compile)
+  "When called interactively with no active region, toggle comment on current line instead."
+  (interactive
+   (if mark-active (list (region-beginning) (region-end))
+     (list (line-beginning-position)
+           (line-beginning-position 2)))))
+
+;;
+(defvar current-date-time-format "%a %b %d %H:%M:%S %Z %Y"
+  "Format of date to insert with `insert-current-date-time' func
+See help of `format-time-string' for possible replacements")
+
+(defvar current-time-format "%a %H:%M:%S"
+  "Format of date to insert with `insert-current-time' func.
+Note the weekly scope of the command's precision.")
+
+(defun insert-current-date-time ()
+  "insert the current date and time into current buffer.
+Uses `current-date-time-format' for the formatting the date/time."
+       (interactive)
+;       (insert (let () (comment-start)))
+       (insert (format-time-string current-date-time-format (current-time)))
+       )
+
+(defun insert-current-time ()
+  "insert the current time (1-week scope) into the current buffer."
+       (interactive)
+       (insert (format-time-string current-time-format (current-time)))
+       )
+
+
+;; Hungry-delete
+(require 'hungry-delete)
+(global-hungry-delete-mode)

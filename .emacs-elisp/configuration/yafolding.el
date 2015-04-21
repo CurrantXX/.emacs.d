a40f7ecf553d4d6a074b2c72d279302a40471f36
GallonChoi <404842441@qq.com>
2015-04-21 21:16:43 +0000

    restruct yafolding.el

--- .emacs-elisp/configuration/yafolding.el
+++ .emacs-elisp/configuration/yafolding.el
@@ -1,5 +1,5 @@
 ;;---------------yafolding-----------------
-(add-to-list 'load-path "~/.emacs-elisp/yafolding.el")
+(add-to-list 'load-path "~/.emacs-elisp/common/yafolding.el")
 (require 'yafolding)
 (define-key global-map (kbd "C-'") 'yafolding-toggle-element)
 (define-key global-map (kbd "C-c C-f") 'yafolding-toggle-all)
